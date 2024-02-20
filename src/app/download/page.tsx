1efdbec7cddafc4d0497f4a3eb6da93592285111
murtazakp <khopoliwala52@gmail.com>
2024-02-20 16:16:06 +0530

    feat(kinde): added download functionaliry

---
+++ src/app/download/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Download = () => {
+  return (
+    <div>
+      <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+              Your Download is Ready
+            </h2>
+            <p className="mt-4 text-2xl font-medium">click below button</p>
+
+            <div className="flex flex-col items-center justify-center px-16 mt-8 space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row lg:mt-12 sm:px-0">
+              <a
+                download
+                href="/pdf/saas.pdf"
+                title=""
+                className="inline-flex items-center justify-center w-full px-8 py-4 text-base font-semibold text-black transition-all duration-200 bg-transparent border border-black rounded-md sm:w-auto hover:bg-black hover:text-white focus:bg-black focus:text-white"
+                role="button"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" />
+                </svg>
+                Download
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Download;
--- src/app/suscribe/page.tsx
+++ src/app/suscribe/page.tsx
@@ -1,96 +1,117 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Download from "../download/page";
 
 const Subscribe = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleSubmit = (event: any) => {
+    setTimeout(() => {
+      setIsSuccess(!isSuccess);
+    }, 2000);
+  };
+
   return (
-    <div className="kuiembed-container">
-      <form
-        method="post"
-        action="https://setoos.kinde.com/widgets/subscribe/v1/subscribe"
-        data-kui-form="true"
-        id="id__kui_form"
-        noValidate
-      >
-        {/* do not remove this or risk form bot signups */}
-        <div
-          style={{ position: "absolute", left: "-5000px" }}
-          aria-hidden="true"
-        >
-          <input
-            data-kui-element="true"
-            type="text"
-            id="id__kui_p_0fb67a0e1d7d47c0b12ec7c0af02dd16"
-            name="p_0fb67a0e1d7d47c0b12ec7c0af02dd16"
-            tabIndex={-1}
-            value=""
-          />
-        </div>
-        <fieldset>
-          <legend>
-            <span className="kuiembed-heading">Stay in the loop</span>
-            <span className="kuiembed-sub-heading">
-              Hear from us about product updates, news and events
-            </span>
-          </legend>
-          <div className="kuiembed-form">
-            <div className="kuiembed-field">
-              <label
-                className="kuiembed-label"
-                data-kui-name="new_name"
-                htmlFor="id__kui_new_name"
-              >
-                First name
-              </label>
+    <>
+      {!isSuccess && (
+        <div className="kuiembed-container">
+          <form
+            onSubmit={handleSubmit}
+            method="post"
+            action="https://setoos.kinde.com/widgets/subscribe/v1/subscribe"
+            data-kui-form="true"
+            id="id__kui_form"
+            noValidate
+          >
+            {/* do not remove this or risk form bot signups */}
+            <div
+              style={{ position: "absolute", left: "-5000px" }}
+              aria-hidden="true"
+            >
               <input
-                className="kuiembed-input-field__name"
+                data-kui-element="true"
                 type="text"
-                id="id__kui_new_name"
-                data-kui-element="true"
-                name="new_name"
+                id="id__kui_p_0fb67a0e1d7d47c0b12ec7c0af02dd16"
+                name="p_0fb67a0e1d7d47c0b12ec7c0af02dd16"
+                tabIndex={-1}
+                value=""
               />
             </div>
-            <div className="kuiembed-field">
-              <label
-                className="kuiembed-label"
-                data-kui-name="new_email"
-                htmlFor="id__kui_new_email"
-              >
-                Email
-              </label>
-              <input
-                className="kuiembed-input-field__email"
-                type="email"
-                id="id__kui_new_email"
-                name="new_email"
+            <fieldset>
+              <legend>
+                <span className="kuiembed-heading">Stay in the loop</span>
+                <span className="kuiembed-sub-heading">
+                  Hear from us about product updates, news and events
+                </span>
+              </legend>
+              <div className="kuiembed-form">
+                <div className="kuiembed-field">
+                  <label
+                    className="kuiembed-label"
+                    data-kui-name="new_name"
+                    htmlFor="id__kui_new_name"
+                  >
+                    First name
+                  </label>
+                  <input
+                    className="kuiembed-input-field__name"
+                    type="text"
+                    id="id__kui_new_name"
+                    data-kui-element="true"
+                    name="new_name"
+                  />
+                </div>
+                <div className="kuiembed-field">
+                  <label
+                    className="kuiembed-label"
+                    data-kui-name="new_email"
+                    htmlFor="id__kui_new_email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="kuiembed-input-field__email"
+                    type="email"
+                    id="id__kui_new_email"
+                    name="new_email"
+                    data-kui-element="true"
+                    autoComplete="email"
+                  />
+                </div>
+                <div className="kuiembed-field">
+                  <button type="submit" className="kuiembed-button">
+                    Download
+                  </button>
+                </div>
+              </div>
+              <p
                 data-kui-element="true"
-                autoComplete="email"
-              />
-            </div>
-            <div className="kuiembed-field">
-              <button type="submit" className="kuiembed-button">
-                Sign up
-              </button>
-            </div>
+                id="id__kui_message"
+                className="kuiembed-message"
+              ></p>
+            </fieldset>
+          </form>
+          <div className="kuiembed-kinde-logo">
+            <p className="kuiembed-kinde-logo__text">
+              Powered by<span className="kuiembed-hide-visually"> Kinde</span>
+            </p>
+            <a href="https://kinde.com" className="kuiembed-kinde-logo__icon">
+              <svg viewBox="0 0 86 28" xmlns="http://www.w3.org/2000/svg">
+                <g transform="translate(0.000300, -0.000468)">
+                  {/* SVG Paths and Polygons */}
+                </g>
+              </svg>
+            </a>
           </div>
-          <p
-            data-kui-element="true"
-            id="id__kui_message"
-            className="kuiembed-message"
-          ></p>
-        </fieldset>
-      </form>
-      <div className="kuiembed-kinde-logo">
-        <p className="kuiembed-kinde-logo__text">
-          Powered by<span className="kuiembed-hide-visually"> Kinde</span>
-        </p>
-        <a href="https://kinde.com" className="kuiembed-kinde-logo__icon">
-          <svg viewBox="0 0 86 28" xmlns="http://www.w3.org/2000/svg">
-            <g transform="translate(0.000300, -0.000468)">
-              {/* SVG Paths and Polygons */}
-            </g>
-          </svg>
-        </a>
-      </div>
-    </div>
+        </div>
+      )}
+
+      {isSuccess && (
+        <div>
+          <Download />
+        </div>
+      )}
+    </>
   );
 };
 
