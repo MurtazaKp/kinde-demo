d99b4bc7d6bfed1e1e3e934fa660a5d3a3c9bc45
murtazakp <khopoliwala52@gmail.com>
2024-02-29 21:00:46 +0530

    fixed

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -1,46 +1,43 @@
 import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import Link from "next/link";
 import React from "react";
+import Google from "../icon/google";
+import Logo from "../icon/logo";
+import X from "../icon/x";
 
 const AuthPage = () => {
   return (
-    <div className="py-10">
-      <div className="flex justify-center flex-col gap-10 items-center">
-        <h2 className="text-lg">Great</h2>
-        <p className="text-3xl">You are just one step away from accessing</p>
-        <p className="text-3xl font-bold">Premium Assets !</p>
+    <div className=" flex justify-center ">
+      <div className="flex justify-center flex-col gap-1 items-center w-full">
+        <h2 className="text-lg">
+          <Link href="/">
+            <Logo />
+          </Link>
+        </h2>
+        <p className="text-2xl w-4/12">
+          Great You are just one step away from accessing premium assets !
+        </p>
+        <div className="mt-10 flex gap-7">
+          <RegisterLink
+            authUrlParams={{
+              connection_id: "conn_7cfefd2f49844de68f5891605b438957",
+            }}
+          >
+            <button className="flex gap-2  w-32 items-center justify-center px-10 py-2 text-base font-medium  transition-all duration-200 bg-gray-100 border border-transparent rounded ">
+              <Google />
+            </button>
+          </RegisterLink>
 
-        <RegisterLink
-          authUrlParams={{
-            connection_id: "conn_7cfefd2f49844de68f5891605b438957",
-          }}
-        >
-          <button className="flex gap-2  w-44 items-center justify-center px-10 py-4 text-base font-medium text-white transition-all duration-200 bg-black border border-transparent rounded ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 48 48"
-              width="30px"
-              height="30px"
-            >
-              <path
-                fill="#FFC107"
-                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-              />
-              <path
-                fill="#FF3D00"
-                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-              />
-              <path
-                fill="#4CAF50"
-                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-              />
-              <path
-                fill="#1976D2"
-                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-              />
-            </svg>{" "}
-            Sign in{" "}
-          </button>
-        </RegisterLink>
+          <RegisterLink
+            authUrlParams={{
+              connection_id: "conn_844934f6325c4fada8ba2cc0b818d6fb",
+            }}
+          >
+            <button className="flex gap-2  w-32 items-center justify-center px-10 py-2.5 text-base font-medium  transition-all duration-200 bg-gray-100 border border-transparent rounded ">
+              <X />
+            </button>
+          </RegisterLink>
+        </div>
       </div>
     </div>
   );
